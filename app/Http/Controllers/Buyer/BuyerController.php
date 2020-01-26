6ce504b9d4acdaffb93aa498d178e5a0bb4c4055
Juan Ramirez <ep.juan@gmail.com>
2020-01-25 19:23:54 -0500

    Haciendo Uso de los Metodos Generalizados

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -17,7 +17,7 @@ class BuyerController extends ApiController
     {
         $compradores = Buyer::has('transactions')->get();
 
-        return response()->json(['data' => $compradores], 200);
+        return $this->showAll($compradores);
     }
     /**
      * Display the specified resource.
@@ -29,6 +29,6 @@ class BuyerController extends ApiController
     {
         $comprador = Buyer::has('transactions')->findOrFail($id);
 
-        return response()->json(['data' => $comprador], 200);
+        return $this->showOne($comprador);
     }
 }
