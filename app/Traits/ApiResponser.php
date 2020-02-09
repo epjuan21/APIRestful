4412c78ba63c4f53b267c29a3541712ccc17a1ba
Juan Ramirez <ep.juan@gmail.com>
2020-02-09 15:57:41 -0500

    Filtrando Respuestas Según Múltiples Atributos

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -24,6 +24,7 @@ trait ApiResponser {
         }
         
         $transformer = $collection->first()->transformer;
+        $collection = $this->filterData($collection, $transformer);
         $collection = $this->sortData($collection, $transformer);
         $collection = $this->transformData($collection, $transformer);
 
@@ -40,6 +41,18 @@ trait ApiResponser {
     protected function showMessage($message, $code = 200)
     {
         return $this->successResponse(['data' => $message], $code);
+    }
+    
+    public function filterData(Collection $collection, $transformer)
+    {
+        foreach (request()->query() as $query => $value) {
+            $attribute = $transformer::originalAttribute($query);
+
+            if (isset($attribute, $value)) {
+                $collection = $collection->where($attribute, $value);
+            }
+        }
+        return $collection;
     }
 
     public function sortData(Collection $collection, $transformer)
