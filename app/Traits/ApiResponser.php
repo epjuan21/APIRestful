5541969335805defc3a4a3d41764dc718e0c9a13
Juan Ramirez <ep.juan@gmail.com>
2020-02-09 17:42:19 -0500

    Implementando el Sistema de Caché

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,6 +3,7 @@
 namespace App\Traits;
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Pagination\LengthAwarePaginator;
@@ -106,6 +107,10 @@ trait ApiResponser {
     protected function cacheResponse($data)
     {
         $url = request()->url();
+
+        return Cache::remember($url, 15/60, function () use($data) {
+            return $data;
+        });
     }
 
 }
