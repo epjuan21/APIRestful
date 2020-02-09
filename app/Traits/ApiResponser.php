33a2bee123abaf263d15139fb6bcc5efba7b737d
Juan Ramirez <ep.juan@gmail.com>
2020-02-09 16:34:33 -0500

    Paginando Colecciones

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 trait ApiResponser {
 
@@ -26,6 +27,7 @@ trait ApiResponser {
         $transformer = $collection->first()->transformer;
         $collection = $this->filterData($collection, $transformer);
         $collection = $this->sortData($collection, $transformer);
+        $collection = $this->paginate($collection);
         $collection = $this->transformData($collection, $transformer);
 
         return $this->successResponse($collection, $code);
@@ -43,7 +45,7 @@ trait ApiResponser {
         return $this->successResponse(['data' => $message], $code);
     }
     
-    public function filterData(Collection $collection, $transformer)
+    protected function filterData(Collection $collection, $transformer)
     {
         foreach (request()->query() as $query => $value) {
             $attribute = $transformer::originalAttribute($query);
@@ -55,7 +57,7 @@ trait ApiResponser {
         return $collection;
     }
 
-    public function sortData(Collection $collection, $transformer)
+    protected function sortData(Collection $collection, $transformer)
     {
         if (request()->has('sort_by')) {
             $attribute = $transformer::originalAttribute(request()->sort_by);
@@ -63,6 +65,21 @@ trait ApiResponser {
             $collection = $collection->sortBy->{$attribute};
         }
         return $collection;
+    }
+
+    protected function paginate(Collection $collection)
+    {
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 15;
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
+
     }
 
     protected function transformData($data, $transformer)
